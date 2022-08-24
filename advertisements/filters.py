68d4a817948291bbe14f711a5ad9d33a5a17374f
User <user@user.com>
2022-08-24 15:15:32 +0300

    Initial commit

---
+++ advertisements/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+from django_filters import rest_framework as filters
+
+from advertisements.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+    created_at = django_filters.DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['created_at', 'creator', 'status']
+
+    # TODO: задайте требуемые фильтры
---
+++ advertisements/views.py
@@ -0,0 +1,26 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_fields = ['created_at', 'status', 'creator']
+    filterset_class = AdvertisementFilter
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        else:
+            return []
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
